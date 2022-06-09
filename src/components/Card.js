10da35389ec82dd6e107855a0e3d57ad71043acf
chubi-x <michaeladejoh01@gmail.com>
2022-06-09 11:26:58 +0100

    changed readme

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react"
+import katie_img from "../images/katie.png"
+import star from "../images/star.png"
+export default function Card() {
+    return (
+       <div className="card-container">
+            <div className="card">
+                <div className="card-status">SOLD OUT</div>
+                <img className="card-image" src= {katie_img}/>
+                <div className="card-info">
+                    <p className="card-rating-info">
+                    <span><img className="card-rating-star" src={star}/> 5.0 (6)</span> · USA
+                    </p>
+                    <p className="card-title">Life lessons with Katie Zaferes fhghfsggfggfwefg</p>
+                    <p> <span className="card-price">From $136 </span>/ person</p>
+                </div>
+            </div>
+        </div>
+    )
+}
