6c169feeb870f55a4553eae01f61f15166afd923
chubi-x <michaeladejoh01@gmail.com>
2022-06-23 13:22:31 +0100

    feat: pass experience information as props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import {experiences} from './data/experiences'
 export default function App() {
             
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            {
+                React.Children.toArray(
+                    experiences.map((item)=>(
+                        <Card
+                        title = {item.title}
+                        price =  {item.price}
+                        rating = {item.rating}
+                        noOfRatings = {item.noOfRatings}
+                        status = {item.status}
+                        image = {item.image}
+                        country= {item.country}
+
+                    />
+                    ))
+                )
+            }
+            
         </div>
     )
 }
--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,12 +1,13 @@
+import img from '../images/katie.png'
 const experiences = [
     {   title: "Life lessons with Katie Zaferes",
         price: 500,
         status: "sold out",
-        image: "../images/katie.png",
-        rating: 5,
+        image: img,
+        rating: 5.0,
         noOfRatings: 10,
         country: "USA",
 
-    }
+    },
 ]
 export {experiences}
