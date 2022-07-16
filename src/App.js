651a5c112220f15424fc87fcd9399337b7462977
chubi-x <chubixax@protonmail.com>
2022-07-16 10:14:13 +0100

    feat: center hero elements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
-import data from './data/data'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data/data";
 export default function App() {
-            
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <div className="card-container">
-            {
-                    data.map((item)=>(
-                        <Card
-                        key = {item.id}
-                        item = {item}
-                    />
-                    ))
-                
-            }
-            </div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Navbar />
+      <div className="hero-container">
+        <Hero />
+      </div>
+      <div className="card-container">
+        {data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import photogrid from "../images/photogrid.png";
+export default function Hero() {
+  return (
+    <section className="hero">
+      <figure className="hero-img-container">
+        <img src={photogrid} className="hero--photo" alt="hero" />
+      </figure>{" "}
+      <h1 className="hero--header"> Online Experiences </h1>{" "}
+      <p className="hero--text">
+        {" "}
+        Join unique interactive activities led by one - of -a - kind hosts— all
+        without leaving home.{" "}
+      </p>{" "}
+    </section>
+  );
+}
